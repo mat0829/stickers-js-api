c4304630750274ef35dc32e371fa69a8f05e7795
mat_anderson <matandersonlv@hotmail.com>
2020-02-21 13:33:58 -0800

    got postgresql working, updated user and task models, and migrated db

--- stickers-js-api-backend/app/models/user.rb
+++ stickers-js-api-backend/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Will return an array of tasks from the parent_user instance
+  has_many :parent_tasks, foreign_key: :task_giver_id, class_name: "Task"
+  
+  # Will return an array of tasks for the child_user instance
+  has_many :child_tasks, foreign_key: :task_receiver_id, class_name: "Task"
+
+  # Will return an array of children who have tasks through the parent_user instance
+  has_many :children, through: :child_tasks, source: :parent_user
 end
