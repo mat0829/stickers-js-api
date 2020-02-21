bd6ff4c6eb887d4b0ba6ba68544ca3a1562d5974
mat_anderson <matandersonlv@hotmail.com>
2020-02-20 16:12:26 -0800

    generated task model and migrations

---
+++ stickers-js-api-backend/db/migrate/20200221001041_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :task_name
+      t.boolean :completed
+      t.integer :sticker_value
+      t.integer :task_receiver_id
+      t.integer :task_giver_id
+
+      t.timestamps
+    end
+  end
+end
