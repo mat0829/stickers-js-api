f071264f96a3048d1b21244aad57da33aba3736a
mat_anderson <matandersonlv@hotmail.com>
2020-02-25 08:32:07 -0800

    added UserSerializer to users_controller create action, password_digest to users migration, and has_secure_password to user model

--- stickers-js-api-backend/app/controllers/api/v1/users_controller.rb
+++ stickers-js-api-backend/app/controllers/api/v1/users_controller.rb
@@ -11,11 +11,11 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: 200
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
     else
-      render json: @user.errors
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
   
--- stickers-js-api-backend/db/schema.rb
+++ stickers-js-api-backend/db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_001041) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
