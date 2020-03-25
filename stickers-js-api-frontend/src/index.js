b1f616757fb6826a0e6c8c099623b21e7541ce67
mat_anderson <matandersonlv@hotmail.com>
2020-03-25 11:56:53 -0700

    added in proper formating for index.js and added in red to delete buttons

--- stickers-js-api-frontend/src/index.js
+++ stickers-js-api-frontend/src/index.js
@@ -47,50 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const editTaskImageInput = document.querySelector('#edit-task-image')
   const editTaskCompletedInput = document.querySelector('#edit-task-completed')
 
-// CREATE A NEW ADULT USER
-  adultUserForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    fetch('http://localhost:3000/api/v1/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', //MIME type we're sending to the server
-         Accept: 'application/json'
-       },
-       body: JSON.stringify({
-         // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
-         user: {
-          name: adultNewNameInput.value,
-          email: adultNewEmailInput.value,
-          password: adultNewPasswordInput.value,
-          avatar: adultNewAvatarInput.value
-         }
-       })
-    })
-    .then((r) => r.json())
-    .then((newUserJSON) => {
-      const number = Math.floor((Math.random() * 100) + 1)
-      if (newUserJSON.user.avatar == '') {
-        userChoice = prompt("Choose between a random Robot, Cat, Dog, Monster Avatar or type in a Noun(person, place, or thing)")
-        if (userChoice == 'Robot' || userChoice == 'robot') {
-          newUserJSON.user.avatar = `https://robohash.org/Random-Robot-Avatar`+`${number}`+`.png` // Generates a random Robot avatar
-        } else if (userChoice == 'Cat' || userChoice == 'cat') {
-          newUserJSON.user.avatar = `https://cataas.com/cat?`+`${number}` // Generates a random Cat avatar
-        } else if (userChoice == 'Dog' || userChoice == 'dog') {
-          newUserJSON.user.avatar = `https://placedog.net/500/280/?id=`+`${number}` // Generates a random Dog avatar
-        } else if (userChoice == 'Monster' || userChoice == 'monster') {
-          newUserJSON.user.avatar = `https://api.adorable.io/avatars/200/`+`${number}`+`.png` // Generates a random Monster avatar
-        } else {
-          newUserJSON.user.avatar = `http://loremflickr.com/320/240/`+`${userChoice}` // Generates an avatar based on the word given
-        }
-        localStorage.setItem("avatar", newUserJSON.user.avatar)
-      }
-      const newUser = new User(newUserJSON)
-      localStorage.setItem("token", newUser.token)
-      localStorage.setItem("parentId", newUser.id)
-      userInfo.innerHTML += newUser.renderWelcomeUser() //render the changes so the DOM is in sync with our data
-    })
-  })
-
 // USER LOGIN
   loginForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -145,85 +101,129 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-// CLICK EDIT USER + PRE-FILL FORM
-    userInfo.addEventListener('click', (event) => {
-      if (event.target.className === 'edit' || event.target.dataset.action === 'edit') {
-        console.log(event.target)
-        const clickedUserId = parseInt(event.target.dataset.id)
-        const foundUser = User.findUser(clickedUserId)
-        // pre-fill the form data:
-        adultEditNameInput.value = foundUser.name
-        adultEditEmailInput.value = foundUser.email
-        adultEditPasswordInput.value = foundUser.password
-        adultEditAvatarInput.value = foundUser.avatar
-        adultEditUserForm.dataset.id = foundUser.id //store the task id in the form so we can PATCH with that id later
-      }
+// CREATE A NEW ADULT USER
+  adultUserForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', //MIME type we're sending to the server
+         Accept: 'application/json'
+       },
+       body: JSON.stringify({
+         // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
+         user: {
+          name: adultNewNameInput.value,
+          email: adultNewEmailInput.value,
+          password: adultNewPasswordInput.value,
+          avatar: adultNewAvatarInput.value
+         }
+       })
     })
+    .then((r) => r.json())
+    .then((newUserJSON) => {
+      const number = Math.floor((Math.random() * 100) + 1)
+      if (newUserJSON.user.avatar == '') {
+        userChoice = prompt("Choose between a random Robot, Cat, Dog, Monster Avatar or type in a Noun(person, place, or thing)")
+        if (userChoice == 'Robot' || userChoice == 'robot') {
+          newUserJSON.user.avatar = `https://robohash.org/Random-Robot-Avatar`+`${number}`+`.png` // Generates a random Robot avatar
+        } else if (userChoice == 'Cat' || userChoice == 'cat') {
+          newUserJSON.user.avatar = `https://cataas.com/cat?`+`${number}` // Generates a random Cat avatar
+        } else if (userChoice == 'Dog' || userChoice == 'dog') {
+          newUserJSON.user.avatar = `https://placedog.net/500/280/?id=`+`${number}` // Generates a random Dog avatar
+        } else if (userChoice == 'Monster' || userChoice == 'monster') {
+          newUserJSON.user.avatar = `https://api.adorable.io/avatars/200/`+`${number}`+`.png` // Generates a random Monster avatar
+        } else {
+          newUserJSON.user.avatar = `http://loremflickr.com/320/240/`+`${userChoice}` // Generates an avatar based on the word given
+        }
+        localStorage.setItem("avatar", newUserJSON.user.avatar)
+      }
+      const newUser = new User(newUserJSON)
+      localStorage.setItem("token", newUser.token)
+      localStorage.setItem("parentId", newUser.id)
+      userInfo.innerHTML += newUser.renderWelcomeUser() //render the changes so the DOM is in sync with our data
+    })
+  })
+
+// CLICK EDIT USER + PRE-FILL FORM
+  userInfo.addEventListener('click', (event) => {
+    if (event.target.className === 'edit' || event.target.dataset.action === 'edit') {
+      console.log(event.target)
+      const clickedUserId = parseInt(event.target.dataset.id)
+      const foundUser = User.findUser(clickedUserId)
+      // pre-fill the form data:
+      adultEditNameInput.value = foundUser.name
+      adultEditEmailInput.value = foundUser.email
+      adultEditPasswordInput.value = foundUser.password
+      adultEditAvatarInput.value = foundUser.avatar
+      adultEditUserForm.dataset.id = foundUser.id //store the task id in the form so we can PATCH with that id later
+    }
+  })
 
 // PATCH REQUEST TO UPDATE USER
-    adultEditUserForm.addEventListener('submit', (event) => {
-      event.preventDefault()
-      const token = localStorage.token
-      const updateUserId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
-      fetch(`http://localhost:3000/api/v1/users/${updateUserId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify({
-          // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
-          user: {
-            name: adultEditNameInput.value,
-            email: adultEditEmailInput.value,
-            password: adultEditPasswordInput.value,
-            avatar: adultEditAvatarInput.value
-          }
-        })
-      })
-      .then((r) => r.json())
-      .then((updatedUserJSON) => {
-        const number = Math.floor((Math.random() * 100) + 1)
-        if (updatedUserJSON.avatar == '') {
-          userChoice = prompt("Choose between a random Robot, Cat, Dog, Monster Avatar or type in a Noun(person, place, or thing)")
-          if (userChoice == 'Robot' || userChoice == 'robot') {
-            updatedUserJSON.avatar = `https://robohash.org/Random-Robot-Avatar`+`${number}`+`.png` // Generates a random Robot avatar
-          } else if (userChoice == 'Cat' || userChoice == 'cat') {
-            updatedUserJSON.avatar = `https://cataas.com/cat?`+`${number}` // Generates a random Cat avatar
-          } else if (userChoice == 'Dog' || userChoice == 'dog') {
-            updatedUserJSON.avatar = `https://placedog.net/500/280/?id=`+`${number}` // Generates a random Dog avatar
-          } else if (userChoice == 'Monster' || userChoice == 'monster') {
-            updatedUserJSON.avatar = `https://api.adorable.io/avatars/200/`+`${number}`+`.png` // Generates a random Monster avatar
-          } else {
-            updatedUserJSON.avatar = `http://loremflickr.com/320/240/`+`${userChoice}` // Generates an avatar based on the word given
-          }
-          localStorage.setItem("avatar", updatedUserJSON.avatar)
+  adultEditUserForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const token = localStorage.token
+    const updateUserId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
+    fetch(`http://localhost:3000/api/v1/users/${updateUserId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
+        user: {
+          name: adultEditNameInput.value,
+          email: adultEditEmailInput.value,
+          password: adultEditPasswordInput.value,
+          avatar: adultEditAvatarInput.value
         }
-        const updatedUser = User.updateUser(updatedUserJSON) //delegate updating tasks to the Task class
-        userInfo.innerHTML = updatedUser.renderUserProfile() //render the changes so the DOM is in sync with our data
       })
     })
+    .then((r) => r.json())
+    .then((updatedUserJSON) => {
+      const number = Math.floor((Math.random() * 100) + 1)
+      if (updatedUserJSON.avatar == '') {
+        userChoice = prompt("Choose between a random Robot, Cat, Dog, Monster Avatar or type in a Noun(person, place, or thing)")
+        if (userChoice == 'Robot' || userChoice == 'robot') {
+          updatedUserJSON.avatar = `https://robohash.org/Random-Robot-Avatar`+`${number}`+`.png` // Generates a random Robot avatar
+        } else if (userChoice == 'Cat' || userChoice == 'cat') {
+          updatedUserJSON.avatar = `https://cataas.com/cat?`+`${number}` // Generates a random Cat avatar
+        } else if (userChoice == 'Dog' || userChoice == 'dog') {
+          updatedUserJSON.avatar = `https://placedog.net/500/280/?id=`+`${number}` // Generates a random Dog avatar
+        } else if (userChoice == 'Monster' || userChoice == 'monster') {
+          updatedUserJSON.avatar = `https://api.adorable.io/avatars/200/`+`${number}`+`.png` // Generates a random Monster avatar
+        } else {
+          updatedUserJSON.avatar = `http://loremflickr.com/320/240/`+`${userChoice}` // Generates an avatar based on the word given
+        }
+        localStorage.setItem("avatar", updatedUserJSON.avatar)
+      }
+      const updatedUser = User.updateUser(updatedUserJSON) //delegate updating tasks to the Task class
+      userInfo.innerHTML = updatedUser.renderUserProfile() //render the changes so the DOM is in sync with our data
+    })
+  })
 
 // DELETE REQUEST TO DELETE USER
-    userInfo.addEventListener('click', (event) => {
-      if (event.target.className === 'delete' || event.target.dataset.action === 'delete') {
-        console.log(event.target)
-        var result = confirm("Are you sure you want to delete this User? Click ok to confirm.");
-        if (result) {
-          const token = localStorage.token
-          const userToDeleteId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
-          fetch(`http://localhost:3000/api/v1/users/${userToDeleteId}`, {
-            method: 'DELETE',
-            headers: {
-              'Content-Type': 'application/json', //MIME type we're sending to the server
-              'Authorization': `Bearer ${token}`
-            }
-          })
-          .then(userInfo.innerHTML = '')
-          .then(window.location.reload(true))
-        }
+  userInfo.addEventListener('click', (event) => {
+    if (event.target.className === 'delete' || event.target.dataset.action === 'delete') {
+      console.log(event.target)
+      var result = confirm("Are you sure you want to delete this User? Click ok to confirm.");
+      if (result) {
+        const token = localStorage.token
+        const userToDeleteId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
+        fetch(`http://localhost:3000/api/v1/users/${userToDeleteId}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json', //MIME type we're sending to the server
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        .then(userInfo.innerHTML = '')
+        .then(window.location.reload(true))
       }
-    })
+    }
+  })
 
 // INITIAL FETCH OF TASKS
   navBar.addEventListener('click', (event) => {
@@ -285,74 +285,74 @@ document.addEventListener('DOMContentLoaded', () => {
    })
 
 // RENDER DETAILS OF CLICKED TASK
-    taskBar.addEventListener('click', (event) => {
-      console.log(event)
-      const clickedTaskId = parseInt(event.target.dataset.id)
-      const foundTask = Task.findTask(clickedTaskId)
-      taskInfo.innerHTML = foundTask.renderDetails()
-    })
+  taskBar.addEventListener('click', (event) => {
+    console.log(event)
+    const clickedTaskId = parseInt(event.target.dataset.id)
+    const foundTask = Task.findTask(clickedTaskId)
+    taskInfo.innerHTML = foundTask.renderDetails()
+  })
 
 // CLICK EDIT TASK + PRE-FILL FORM
-    taskInfo.addEventListener('click', (event) => {
-      if (event.target.className === 'edit' || event.target.dataset.action === 'edit') {
-        console.log(event.target)
-        const clickedTaskId = parseInt(event.target.dataset.id)
-        const foundTask = Task.findTask(clickedTaskId) //find the task object based on the id found in the clicked edit button
-        // pre-fill the form data:
-        editTaskNameInput.value = foundTask.name
-        editTaskValueInput.value = foundTask.value
-        editTaskImageInput.value = foundTask.image
-        editTaskCompletedInput.checked = foundTask.completed
-        editTaskForm.dataset.id = foundTask.id //store the task id in the form so we can PATCH with that id later
-      }
-    })
+  taskInfo.addEventListener('click', (event) => {
+    if (event.target.className === 'edit' || event.target.dataset.action === 'edit') {
+      console.log(event.target)
+      const clickedTaskId = parseInt(event.target.dataset.id)
+      const foundTask = Task.findTask(clickedTaskId) //find the task object based on the id found in the clicked edit button
+      // pre-fill the form data:
+      editTaskNameInput.value = foundTask.name
+      editTaskValueInput.value = foundTask.value
+      editTaskImageInput.value = foundTask.image
+      editTaskCompletedInput.checked = foundTask.completed
+      editTaskForm.dataset.id = foundTask.id //store the task id in the form so we can PATCH with that id later
+    }
+  })
 
 // PATCH REQUEST TO UPDATE TASK
-    editTaskForm.addEventListener('submit', (event) => {
-      console.log(event)
-      event.preventDefault()
-      const token = localStorage.token
-      const updateTaskId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
-      fetch(`http://localhost:3000/api/v1/tasks/${updateTaskId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify({
-          // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
-          name: editTaskNameInput.value,
-          value: editTaskValueInput.value,
-          image: editTaskImageInput.value,
-          completed: editTaskCompletedInput.checked
-        })
-      })
-      .then((r) => r.json())
-      .then((updatedTaskJSON) => {
-        const updatedTask = Task.updateTask(updatedTaskJSON) //delegate updating tasks to the Task class
-        taskInfo.innerHTML = updatedTask.renderDetails() //render the changes so the DOM is in sync with our data
+  editTaskForm.addEventListener('submit', (event) => {
+    console.log(event)
+    event.preventDefault()
+    const token = localStorage.token
+    const updateTaskId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
+    fetch(`http://localhost:3000/api/v1/tasks/${updateTaskId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
+        name: editTaskNameInput.value,
+        value: editTaskValueInput.value,
+        image: editTaskImageInput.value,
+        completed: editTaskCompletedInput.checked
       })
     })
+    .then((r) => r.json())
+    .then((updatedTaskJSON) => {
+      const updatedTask = Task.updateTask(updatedTaskJSON) //delegate updating tasks to the Task class
+      taskInfo.innerHTML = updatedTask.renderDetails() //render the changes so the DOM is in sync with our data
+    })
+  })
 
 // DELETE REQUEST TO DELETE TASK
-    taskInfo.addEventListener('click', (event) => {
-      if (event.target.className === 'delete' || event.target.dataset.action === 'delete') {
-        console.log(event.target)
-        debugger
-        var result = confirm("Are you sure you want to delete this Task? Click ok to confirm.");
-        if (result) {
-          const token = localStorage.token
-          const taskToDeleteId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
-          fetch(`http://localhost:3000/api/v1/tasks/${taskToDeleteId}`, {
-            method: 'DELETE',
-            headers: {
-              'Content-Type': 'application/json', //MIME type we're sending to the server
-              'Authorization': `Bearer ${token}`
-            }
-          })
-          .then((tasksBtn).click())
-          .then(taskInfo.innerHTML = '')
-        }
+  taskInfo.addEventListener('click', (event) => {
+    if (event.target.className === 'delete' || event.target.dataset.action === 'delete') {
+      console.log(event.target)
+      debugger
+      var result = confirm("Are you sure you want to delete this Task? Click ok to confirm.");
+      if (result) {
+        const token = localStorage.token
+        const taskToDeleteId = event.target.dataset.id //don't need to parseInt because we are interpolating the id into a url string
+        fetch(`http://localhost:3000/api/v1/tasks/${taskToDeleteId}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json', //MIME type we're sending to the server
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        .then((tasksBtn).click())
+        .then(taskInfo.innerHTML = '')
       }
-    })
+    }
+  })
 })
