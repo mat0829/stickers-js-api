ee4c1ddc6242d337390437967f239a3b3d0be9cc
mat_anderson <matandersonlv@hotmail.com>
2020-05-12 09:47:16 -0700

    updated avatarCreationIfEmpty to generate numbers based on each sets amount of possibilities

--- stickers-js-api-frontend/src/index.js
+++ stickers-js-api-frontend/src/index.js
@@ -172,17 +172,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function avatarCreationIfEmpty(jsonUserData) {
-    const number = Math.floor((Math.random() * 100) + 1)
+    const robotNumber = Math.floor((Math.random() * 1000) + 1)
+    const catNumber = Math.floor((Math.random() * 415) + 1)
+    const dogNumber = Math.floor((Math.random() * 150) + 1)
+    const monsterNumber = Math.floor((Math.random() * 750) + 1)
     if (jsonUserData.avatar == '') {
       userChoice = prompt("Choose between a random Robot, Cat, Dog, Monster Avatar or type in a Noun(person, place, or thing)")
         if (userChoice == 'Robot' || userChoice == 'robot') {
-          jsonUserData.avatar = `https://robohash.org/Random-Robot-Avatar`+`${number}`+`.png` // Generates a random Robot avatar
+          jsonUserData.avatar = `https://robohash.org/Random-Robot-Avatar`+`${robotNumber}`+`.png` // Generates a random Robot avatar
         } else if (userChoice == 'Cat' || userChoice == 'cat') {
-            jsonUserData.avatar = `https://cataas.com/cat?`+`${number}` // Generates a random Cat avatar
+            jsonUserData.avatar = `https://cataas.com/cat?`+`${catNumber}` // Generates a random Cat avatar
         } else if (userChoice == 'Dog' || userChoice == 'dog') {
-            jsonUserData.avatar = `https://placedog.net/500/280/?id=`+`${number}` // Generates a random Dog avatar
+            jsonUserData.avatar = `https://placedog.net/500/280/?id=`+`${dogNumber}` // Generates a random Dog avatar
         } else if (userChoice == 'Monster' || userChoice == 'monster') {
-            jsonUserData.avatar = `https://api.adorable.io/avatars/200/`+`${number}`+`.png` // Generates a random Monster avatar
+            jsonUserData.avatar = `https://api.adorable.io/avatars/200/`+`${monsterNumber}`+`.png` // Generates a random Monster avatar
         } else {
             jsonUserData.avatar = `http://loremflickr.com/320/240/`+`${userChoice}` // Generates an avatar based on the word given
         }
